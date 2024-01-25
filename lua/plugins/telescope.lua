54d5e78600471340fad3a789a1bbec81fc7e8bf5
Aleksandar Rangelov <122702818+aleksandar-rangelov@users.noreply.github.com>
2024-01-25 21:35:39 +0200

    asdfasdf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
     tag = "0.1.5",
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
     },
     config = function()
       require("telescope").setup({
