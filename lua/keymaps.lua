79abb708f4ef70505411c66a1a3278774e500a75
Aleksandar Rangelov <122702818+aleksandar-rangelov@users.noreply.github.com>
2024-03-17 21:22:15 +0200

    keymap and gruvbox changesw

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -24,15 +24,15 @@ vim.api.nvim_set_keymap('v', '<C-/>', 'gcc', { silent = true })
 vim.keymap.set('n', 'U', '<C-r>', opts)
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', '<cmd> TmuxNavigateDown<CR>', opts)
-vim.keymap.set('n', '<c-j>', '<cmd> TmuxNavigateUp<CR>', opts)
-vim.keymap.set('n', '<c-h>', '<cmd> TmuxNavigateLeft<CR>', opts)
-vim.keymap.set('n', '<c-l>', '<cmd> TmuxNavigateRight<CR>', opts)
+-- vim.keymap.set('n', '<c-k>', '<cmd> TmuxNavigateDown<CR>', opts)
+-- vim.keymap.set('n', '<c-j>', '<cmd> TmuxNavigateUp<CR>', opts)
+-- vim.keymap.set('n', '<c-h>', '<cmd> TmuxNavigateLeft<CR>', opts)
+-- vim.keymap.set('n', '<c-l>', '<cmd> TmuxNavigateRight<CR>', opts)
 
--- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
--- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
--- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
--- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Window Management
 vim.keymap.set('n', '<leader>sv', ':vsplit<CR>', opts) -- Split Vertically
@@ -65,9 +65,9 @@ vim.keymap.set('i', '<C-c>', '<Esc>')
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+-- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
